36933dca9a17073805e9deb03d8f0caa79ec53f4
bipoe <bipoeman12@gmail.com>
2026-03-11 04:35:02 +0700

    indents

--- inc/protocol_utils.h
+++ inc/protocol_utils.h
@@ -13,7 +13,7 @@
 // extern CRC_HandleTypeDef hcrc;
 
 #define KISS_CMD_REQUEST_FRAME          0x00
-#define KISS_CMD_DATA_FRAME         0x01
+#define KISS_CMD_DATA_FRAME             0x01
 
 uint32_t KISS_UpdateCRC32(uint32_t crc, const uint8_t *data, uint16_t length);
 uint32_t KISS_CalculateCRC32(const uint8_t *data, uint16_t length);
